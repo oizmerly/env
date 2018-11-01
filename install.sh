e5db83affee8ba35837bb7bf3fab0e7b978ec37b
Oleg Izmerly <bigimotech@gmail.com>
2018-11-01 08:04:57 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# ZSH
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 brew install reattach-to-user-namespace
 
 # zsh autocomplete
